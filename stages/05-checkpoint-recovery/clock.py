a8a085207dcd6c9521cb9d89ab06d5d698275400
andrew <2652020872@qq.com>
2026-08-20 03:36:22 -0700

    chore: remove staged learning implementations

--- stages/05-checkpoint-recovery/clock.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-class LocalClock:
-    """Provide the current time from the machine running the Harness."""
-
-    def current_time(self) -> str:
-        return datetime.now().astimezone().isoformat(timespec="seconds")
